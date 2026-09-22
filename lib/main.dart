e0c4a2f7f8476c1156426c719ce773fdd88b43c6
gakpa boris leonce <61884514+gborisl94@users.noreply.github.com>
2026-09-22 17:52:48 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(MahadevApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MahadevApp());
+}
 
 class MahadevApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Mahadev Player',
+      theme: ThemeData.dark(),
       home: WebViewScreen(),
     );
   }
@@ -20,32 +25,38 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController controller;
-  bool loading = true;
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(Colors.black)
-      ..enableZoom(false)
+      ..setBackgroundColor(const Color(0xFF000000))
       ..setNavigationDelegate(
         NavigationDelegate(
-          onPageStarted: (_) => setState(() => loading = true),
-          onPageFinished: (_) => setState(() => loading = false),
+          onPageStarted: (url) => setState(() => isLoading = true),
+          onPageFinished: (url) => setState(() => isLoading = false),
+          onWebResourceError: (error) {
+            print("Erreur WebView: $error");
+          },
         ),
       )
-      ..loadRequest(Uri.parse('https://mahadevbook.com'));
+      ..loadRequest(Uri.parse('https://mahadevbook.com/'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Mahadev Player'), backgroundColor: Colors.black),
+      appBar: AppBar(
+        title: Text('Mahadev Player'),
+        backgroundColor: Colors.black,
+      ),
       body: Stack(
         children: [
           WebViewWidget(controller: controller),
-          if (loading) Center(child: CircularProgressIndicator()),
+          if (isLoading)
+            Center(child: CircularProgressIndicator(color: Colors.orange)),
         ],
       ),
     );
